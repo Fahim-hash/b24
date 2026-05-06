93efaf768d1cc947f21bb7599d10e05c316492b7
Fahim-hash <82995127+Fahim-hash@users.noreply.github.com>
2026-05-06 09:53:05 +0600

    Update route.ts

--- Documents/backbenchers-hub/api/send-ticket/route.ts
+++ Documents/backbenchers-hub/api/send-ticket/route.ts
@@ -2,7 +2,7 @@ import { Resend } from 'resend';
 import { NextResponse } from 'next/server';
 import { jsPDF } from "jspdf";
 
-const resend = new Resend('re_USBt3RXV_Ma64Cp2CLLSZoDCyREukNz2n');
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
